086a0bd8be27d691fd79f37aa9f5c583d7c43639
davien21 <chidiebereekennia@gmail.com>
2020-11-16 21:29:31 +0100

    add get and post routes

--- routes/debts.js
+++ routes/debts.js
@@ -1,6 +1,6 @@
-const { Debt,validateDebt } = require('../models/debt')
+const { Debt, validateDebt } = require('../models/debt')
 const _ = require('lodash')
-const auth = require('../middleware/auth')
+
 const validateBody = require('../middleware/validateBody')
 const validateObjectId = require('../middleware/validateObjectId')
 
@@ -8,19 +8,25 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  res.send('get debt')
+  const debts = await Debt.find()
+
+  res.send(debts)
 })
 
 router.get('/:id', validateObjectId, async (req, res) => {
-  const debt = await Debt.findById(req.params.id).select('name day price')
+  const debt = await Debt.findById(req.params.id).select('name cause amount')
 
 	if (!debt) return res.status(404).send('Invalid Debt')
 
   res.send(debt)
 })
  
-router.post('/', [auth, validateBody(validateDebt)], async (req, res) => {
-  res.send('debt')
+router.post('/', [validateBody(validateDebt)], async (req, res) => {
+  let debt = new Debt( _.pick(req.body, ['name', 'cause', 'amount'] ))
+  
+  debt = await debt.save()
+
+  res.send(debt)
 })
  
 module.exports = router;

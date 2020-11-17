0c13d20fc7242cb4df0bf699e401d34237aa8629
davien21 <chidiebereekennia@gmail.com>
2020-11-17 14:39:55 +0100

    add cors

--- startup/routes.js
+++ startup/routes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const winston = require('winston')
 const debts = require('../routes/debts')
+const cors = require('cors')
 
 const error = require('../middleware/error')
 
 module.exports = function(app) {
 
   app.use(express.json());
+  app.use(cors())
   app.use('/api/debts', debts) // use the debts router;
   
   //error middleware

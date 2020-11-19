10fda4228aa2537af982b68ef5051d547bfa6ef3
davien21 <chidiebereekennia@gmail.com>
2020-11-19 21:49:40 +0100

    add auth router

--- startup/routes.js
+++ startup/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const winston = require('winston')
 const debts = require('../routes/debts')
+const auth = require('../routes/auth')
 const cors = require('cors')
 
 const error = require('../middleware/error')
@@ -9,6 +10,7 @@ module.exports = function(app) {
 
   app.use(express.json());
   app.use(cors())
+  app.use('/api/auth', auth) // use the auth router;
   app.use('/api/debts', debts) // use the debts router;
   
   //error middleware

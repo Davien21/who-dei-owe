1e2a13944e556b4334e704debffbe64f55bd4224
davien21 <chidiebereekennia@gmail.com>
2020-11-19 21:57:33 +0100

    update auth route to check registered admins

--- routes/auth.js
+++ routes/auth.js
@@ -1,28 +1,32 @@
-const { Admin, validateAdmin } = require('../models/admin')
+const Joi = require('joi');
+const passwordComplexity = require('joi-password-complexity');
 
-const bcrypt = require('bcrypt')
-const _ = require('lodash')
-
-const validateBody = require('../middleware/validateBody')
-const validateObjectId = require('../middleware/validateObjectId')
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
+const { Admin } = require('../models/admin')
 
 const express = require('express');
+const validateBody = require('../middleware/validateBody');
 const router = express.Router();
- 
- 
-router.post('/', [validateBody(validateAdmin)], async (req,res) => {
-  let admin =  await Admin.findOne({ email: req.body.email});
-  if (admin) return res.status(400).send('Admin already exists');
-  
-  admin = new Admin(_.pick(req.body,['name','email','password']));
-  const salt = await bcrypt.genSalt(10);
-  admin.password = await bcrypt.hash(admin.password,salt);
 
-  await admin.save();
+router.post('/', validateBody(validate), async (req,res) => {
+  let admin = await Admin.findOne({ email: req.body.email });
+  if (!admin) return res.status(400).send('Invalid email or password');
   
-  const token =  admin.generateAuthToken();
-	res.header('x-auth-token', token).send(_.pick(admin,['_id','name','email']));
+  const validPassword =  await bcrypt.compare(req.body.password, admin.password);
+  if(!validPassword) return res.status(400).send('Invalid email or password');
+  
+  const token = admin.generateAuthToken();
+  
+  res.send({ authToken: token });
 })
 
- 
+router.validate = validate = (user) => {
+	const schema = Joi.object({
+    email: Joi.string().min(5).max(255).required().email(),
+    password: new passwordComplexity().required(),
+  });
+  return schema.validate(user);
+}
+
 module.exports = router;
